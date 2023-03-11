0893b97782f29733b6d22440f5bfbe028722208b
Dominik Toth <dominik8655@gmail.com>
2023-03-11 19:01:40 +0100

    feat: Have some sort of functioning question system

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import sys, pygame
+from typing import Union
 from random import randint
+from pygame.font import Font
 from pygame.rect import Rect
 from pygame.sprite import Sprite
 from pygame.surface import Surface
@@ -11,28 +13,17 @@ BUTTONS_PLACE = 150
 TEACHERS = ["kisova", "hladka", "horvath", "laurinska", "palenikova", "zilka"]
 QUESTIONS = {
     "kisova": [
-        {"question": "Kolik je 2+2?", "answers": ["2", "3", "4"], "correct": 2},
-        {"question": "Kolik je 2+3?", "answers": ["2", "3", "5"], "correct": 2},
-        {"question": "Kolik je 2+4?", "answers": ["6", "3", "8"], "correct": 1},
-        {"question": "Kolik je 2+5?", "answers": ["2", "7", "4"], "correct": 2},
-        {"question": "Kolik je 2+6?", "answers": ["2", "3", "8"], "correct": 2},
-        {"question": "Kolik je 2+7?", "answers": ["9", "0", "4"], "correct": 0},
+        {
+            "question": "Kisova je 2+2?",
+            "answers": ["Giovanni Giorgo", "Marcus Aurelius", "Napoleon Bonaparte"],
+            "correct": 2,
+        },
     ],
     "hladka": [
-        {"question": "Kolik je 2+2?", "answers": ["2", "3", "4"], "correct": 2},
-        {"question": "Kolik je 2+3?", "answers": ["2", "3", "5"], "correct": 2},
-        {"question": "Kolik je 2+4?", "answers": ["6", "3", "8"], "correct": 1},
-        {"question": "Kolik je 2+5?", "answers": ["2", "7", "4"], "correct": 2},
-        {"question": "Kolik je 2+6?", "answers": ["2", "3", "8"], "correct": 2},
-        {"question": "Kolik je 2+7?", "answers": ["2", "0", "4"], "correct": 0},
+        {"question": "Hladka je 2+2?", "answers": ["2", "3", "4"], "correct": 2},
     ],
     "horvath": [
-        {"question": "Kolik je 2+2?", "answers": ["2", "3", "4"], "correct": 2},
-        {"question": "Kolik je 2+3?", "answers": ["2", "3", "5"], "correct": 2},
-        {"question": "Kolik je 2+4?", "answers": ["6", "3", "8"], "correct": 1},
-        {"question": "Kolik je 2+5?", "answers": ["2", "7", "4"], "correct": 2},
-        {"question": "Kolik je 2+6?", "answers": ["2", "3", "8"], "correct": 2},
-        {"question": "Kolik je 2+7?", "answers": ["2", "0", "4"], "correct": 0},
+        {"question": "Horvath je 2+2?", "answers": ["2", "3", "4"], "correct": 2},
     ],
     "laurinska": [
         {"question": "Kolik je 2+2?", "answers": ["2", "3", "4"], "correct": 2},
@@ -60,21 +51,55 @@ QUESTIONS = {
     ],
 }
 
+pause = False
+
 
 class Button(Sprite):
-    def __init__(self, image_file: str, position: tuple[int, int], question: dict):
+    def __init__(self, text: str, correct: bool, font: Font, i: int, teacher, sprites):
         Sprite.__init__(self)
-        self.image = pygame.image.load(image_file)
-        self.rect = self.image.get_rect(x=position[0], y=position[1])
-        self.question = question
+        self.debug = text
+        self.text = font.render(text, True, (100, 100, 200))
+        text_rect = self.text.get_rect()
+        self.rect = Rect(
+            # Center the buttons
+            # So baaaad
+            50 if i == 0 else WIDTH // 2 - text_rect.width // 2 if i == 1 else WIDTH - (50 + text_rect.width + 20),
+            HEIGHT - BUTTONS_PLACE + 50,
+            text_rect.width + 20,
+            text_rect.height + 20,
+        )
+        self.correct = correct
+        self.teacher = teacher
+        self.sprites = sprites
+
+    def die(self):
+        print(f"Killing {self.debug}")
+        self.kill()
+        self.sprites.remove(self)
 
     def draw(self, screen: Surface):
-        screen.blit(self.image, self.rect)
+        pygame.draw.rect(screen, (100, 200, 100), self.rect, 0, 10)
+        screen.blit(self.text, (self.rect.x + 10, self.rect.y + 10))
 
-    def check_click(self) -> bool:
-        mouse_pos = pygame.mouse.get_pos()
-        # Also check for click (key event)
-        return self.rect.collidepoint(mouse_pos)
+    def on_click(self):
+        if self.correct:
+            self.teacher.die()
+            # Remove unused elements
+            # So baaaad
+            for _ in range(4):
+                if (self.sprites[-1] is not self) and (
+                    (type(self.sprites[-1]) is Button) or type(self.sprites[-1]) is Question
+                ):
+                    self.sprites.remove(self.sprites[-1])
+                elif (self.sprites[-2] is not self) and (
+                    (type(self.sprites[-2]) is Button) or type(self.sprites[-2]) is Question
+                ):
+                    self.sprites.remove(self.sprites[-2])
+            self.die()
+            global pause
+            pause = False
+        else:
+            print("Wrong answer!")
 
 
 class MovingGameObject(Sprite):
@@ -85,7 +110,8 @@ class MovingGameObject(Sprite):
         self.speed = speed
 
     def update(self):
-        self.rect = self.rect.move(self.speed)
+        if not pause:
+            self.rect = self.rect.move(self.speed)
 
     def draw(self, screen: Surface):
         screen.blit(self.image, self.rect)
@@ -124,22 +150,51 @@ class Player(MovingGameObject):
             self.speed[0] = 5
 
 
-class Teacher(MovingGameObject):
-    def __init__(self, image_file: str):
-        temp_sprite = pygame.image.load(image_file).get_rect()
-        super().__init__(image_file, (randint(0, WIDTH - temp_sprite.width), randint(0, 20)), [0, 2])
-        del temp_sprite
+class Question(Sprite):
+    def __init__(self, question_text: str, font: Font):
+        Sprite.__init__(self)
+        self.question_text = question_text
+        self.text = font.render(self.question_text, True, (100, 200, 100))
+        self.text_rect = self.text.get_rect()
+        self.text_rect.center = (WIDTH // 2, HEIGHT - (BUTTONS_PLACE + 20))
         self.dead = False
-        self.name = image_file.split("/")[-1].split(".")[0]
 
     def die(self):
         self.dead = True
         self.kill()
 
+    def draw(self, screen: Surface):
+        screen.blit(self.text, self.text_rect)
+
+
+class Teacher(MovingGameObject):
+    def __init__(self, image_file: str, sprites, big_font: Font, small_font: Font):
+        temp_sprite = pygame.image.load(image_file).get_rect()
+        super().__init__(image_file, (randint(0, WIDTH - temp_sprite.width), randint(0, 20)), [0, 2])
+        del temp_sprite
+        self.dead = False
+        self.name = image_file.split("/")[-1].split(".")[0]
+        self.sprites = sprites
+        self.big_font = big_font
+        self.small_font = small_font
+
+    def die(self):
+        self.kill()
+        self.sprites.remove(self)
+
     def ask(self):
-        pass
+        global pause
+        pause = True
+        question = QUESTIONS[self.name][randint(0, len(QUESTIONS[self.name]) - 1)]
+        self.sprites.append(Question(question["question"], self.big_font))
+        for i in range(3):
+            self.sprites.append(
+                Button(question["answers"][i], i == question["correct"], self.small_font, i, self, self.sprites)
+            )
 
     def update(self, player_rect: Rect):
+        if pause:
+            return
         super().update()
         collision = self.rect.colliderect(player_rect)
         if collision:
@@ -155,11 +210,15 @@ class Game:
         time = 0
         multiplier = 10
         clock = pygame.time.Clock()
-        self.screen = pygame.display.set_mode(SIZE, 0, 32)
+        big_font = pygame.font.SysFont("Arial", 20)
+        small_font = pygame.font.SysFont("Arial", 15)
+        screen = pygame.display.set_mode(SIZE, 0, 32)
         pygame.display.set_caption("Gymgol Simulator")
         pygame.display.set_icon(pygame.image.load("./assets/icon.png"))
 
-        self.sprites = [Player("./assets/sprites/student/student.png"), Teacher("./assets/sprites/teachers/kisova.png")]
+        sprites: list[Union[Player, Teacher, Button]] = [
+            Player("./assets/sprites/student/student.png"),
+        ]
 
         while True:
             time += 1
@@ -169,28 +228,41 @@ class Game:
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_ESCAPE:
                         sys.exit()
-                    for sprite in self.sprites:
-                        if hasattr(sprite, "on_key_down"):
+                    for sprite in sprites:
+                        if type(sprite) == Player:
                             sprite.on_key_down(event.key)
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    for sprite in sprites:
+                        if type(sprite) == Button and sprite.rect.collidepoint(event.pos):
+                            sprite.on_click()
 
-            self.screen.fill(BACKGROUND_COLOR)
+            screen.fill(BACKGROUND_COLOR)
 
-            for sprite in self.sprites:
-                if hasattr(sprite, "dead"):
-                    sprite.update(self.sprites[0].rect)
-                else:
+            for sprite in sprites:
+                if type(sprite) == Teacher:
+                    sprite.update(sprites[0].rect)
+                elif type(sprite) == Player or type(sprite) == Button:
                     sprite.update()
-                sprite.draw(self.screen)
-                if hasattr(sprite, "dead") and sprite.dead:
-                    self.sprites.remove(sprite)
+                sprite.draw(screen)
+                if (type(sprite) == Teacher or type(sprite) == Question) and sprite.dead:
+                    sprites.remove(sprite)
                     if multiplier > 2:
                         multiplier -= 1
 
             pygame.display.flip()
             clock.tick(60)
 
-            if time % (60 * multiplier) == 0:
-                self.sprites.append(Teacher(f"./assets/sprites/teachers/{TEACHERS[randint(0, len(TEACHERS) - 1)]}.png"))
+            if time == 1:
+                sprites.append(Teacher("./assets/sprites/teachers/kisova.png", sprites, big_font, small_font))
+            if time % (60 * multiplier) == 0 and not pause:
+                sprites.append(
+                    Teacher(
+                        f"./assets/sprites/teachers/{TEACHERS[randint(0, len(TEACHERS) - 1)]}.png",
+                        sprites,
+                        big_font,
+                        small_font,
+                    )
+                )
 
 
 game = Game()
